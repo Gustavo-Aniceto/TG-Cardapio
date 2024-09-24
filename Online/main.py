423227f282b86d58f7cda304a1f087f39dda2583
Gustavo Henrique Aniceto <108236975+Gustavo-Aniceto@users.noreply.github.com>
2024-09-24 20:36:05 -0300

    iniciando flask para api

---
+++ Online/main.py
@@ -0,0 +1,20 @@
+from flask import flask, render_template # type: ignore
+from apimercadopago import gerar_link_pagamento
+
+app = flask(__name__)
+
+@app.route("/")
+def homepage():
+    link_iniciar_pagamento = gerar_link_pagamento()
+    return render_template("index.html", link_pagamento=link_iniciar_pagamento)
+
+@app.route("/compracerta")
+def compra_certa():
+    return render_template("compracerta.html")
+
+@app.route("/compraerrada")
+def compra_errada():
+    return render_template("compraerrada.html")
+
+if __name__=="__main__":
+    app.run()
